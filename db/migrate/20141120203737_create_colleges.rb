170398f091f4f4510733f053e4a00f3eafd7389a
Monica Olinescu <monica.olinescu@gmail.com>
2014-11-20 12:41:55 -0800

    Initial commit

---
+++ db/migrate/20141120203737_create_colleges.rb
@@ -0,0 +1,11 @@
+class CreateColleges < ActiveRecord::Migration
+  def change
+    create_table :colleges do |t|
+      t.string :name
+      t.string :address
+      t.integer :student_total
+
+      t.timestamps
+    end
+  end
+end
